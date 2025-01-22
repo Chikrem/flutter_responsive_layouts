c11d9bf242904615e267b6d87070664fb55bb482
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-01-22 10:54:44 -0300

    Criando tela de Drinks com GridView

--- lib/screens/bebidas.dart
+++ lib/screens/bebidas.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_layouts/cardapio.dart';
+import 'package:flutter_responsive_layouts/components/drink_item.dart';
 
 class Bebidas extends StatelessWidget {
-  const Bebidas({super.key});
+  Bebidas({super.key});
+
+  final List items = drinks;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+      ),
+      itemBuilder: (context, index){
+        return DrinkItem(
+          imageURI: items[index]["image"],
+          itemTitle: items[index]["name"],
+          itemPrice: items[index]["price"],
+        );
+      },
+      itemCount: items.length,
+    );
   }
 }
